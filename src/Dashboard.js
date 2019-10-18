4177ef313d5034066f60bf21764980396412a248
pranjitborah99@gmail.com <pranjitborah99@gmail.com>
2019-10-18 17:59:12 +0530

    bug fix

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Header from './Common/Header'; 
 import Footer from './Common/Footer'; 
+import Chart from "react-apexcharts";
 
 
 export default class Dashboard extends React.Component{
 	constructor(props) {
         super(props);
-        
+    
+        this.state = {
+          options: {
+            chart: {
+              id: "basic-bar"
+            },
+            xaxis: {
+              categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+            }
+          },
+          series: [
+            {
+              name: "series-1",
+              data: [30, 40, 45, 50, 49, 60, 70, 91]
+            }
+          ]
+        };
       }
     
     render(){
+		
 	return(
     <div>
             
@@ -21,12 +39,42 @@ export default class Dashboard extends React.Component{
                             Dashboard
                         </div>
                         <div className="panel-body">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <div className="col-lg-12">
+                            <div className="col-lg-4 col-sm-6">
+                            <div className="circle-tile ">
+                                <div className="circle-tile-content dark-blue">
+                                <div className="circle-tile-description text-faded">Copra Receiption</div>
+                                <div className="circle-tile-number text-faded ">265 KG</div>
+                                </div>
+                            </div>
+                            </div>
+                            <div className="col-lg-4 col-sm-6">
+                            <div className="circle-tile ">
+                                <div className="circle-tile-content red">
+                                <div className="circle-tile-description text-faded">Oil Extracting</div>
+                                <div className="circle-tile-number text-faded ">1000 KG</div>
+                                </div>
+                            </div>
+                            </div> 
+                            <div className="col-lg-4 col-sm-6">
+                            <div className="circle-tile ">
+                                <div className="circle-tile-content packingcolr">
+                                <div className="circle-tile-description text-faded">Packing</div>
+                                <div className="circle-tile-number text-faded ">10 KG</div>
+                                </div>
+                            </div>
+                            </div> 
+                        </div>
+						<hr/>
+						<div className="mixed-chart">
+                        <Chart
+                            options={this.state.options}
+                            series={this.state.series}
+                            type="line"
+                            width="100%"
+                            height="400px"
+                        />
+                        </div>
                         </div>
                     </div>
                 </div>
